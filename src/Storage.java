5098820fec2b559bde0dd754b8e6f702f6e9964d
Roberto Inconis <robertoinconis1607@gmail.com>
2023-11-02 18:03:59 +0100

    print products list method

--- src/Storage.java
+++ src/Storage.java
@@ -6,6 +6,23 @@ public class Storage {
 
     public Storage(Product prod) //to do: check if product is already present: increase quantity
     {
+        if(productsList.contains(prod)){
+            productsList.indexOf(prod).getQuantity() + prod.getQuantity();
+            return;
+        }
         productsList.add(prod);
+    }
+
+    public void printProducts()
+    {
+        if(productsList.isEmpty()){
+            System.out.println("No products in storage.");
+            return;
+        }
+        
+        System.out.println("Products list:");
+        for(var values : productsList){
+            System.out.println(values);
+        }
     }
 }
